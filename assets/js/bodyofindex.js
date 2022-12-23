0ea8227037e42854e6671e23ccdcdcb0eb7a568d
merlinlcb <merlinlcb@merlinlcb.com>
2022-12-23 06:22:15 -0700

    code cleanup - asset removal

--- assets/js/bodyofindex.js
+++ assets/js/bodyofindex.js
@@ -5,69 +5,72 @@ function init() {
   Papa.parse(public_spreadsheet_url, {
     download: true,
     header: true,
-    complete: showInfo,
-  });
-}
-
-function init2() {
-  Papa.parse(public_spreadsheet_url, {
-    download: true,
-    header: true,
-    complete: showInfo2,
+    complete: sortlogic,
   });
 }
 
 window.addEventListener("DOMContentLoaded", init);
 window.addEventListener("DOMContentLoaded", init2);
 
-function showInfo(results) {
+function sortlogic(results) {
   var data = results.data;
   var mtgcfirstbox = "";
   var mtgcsecondbox = "";
   var mtgcthirdbox = "";
   var mtgcfourthbox = "";
-  let valuesAlreadySeenOnce = "";
-  let valuesAlreadySeenTwice = "";
-  let valuesAlreadySeenThrice = "";
-  let valuesAlreadySeenQuad = "";
-  let secondresults = false;
-  let thirdresults = false;
-  let fourthresults = false;
+  let mtgcvaluesAlreadySeenOnce = "";
+  let mtgcvaluesAlreadySeenTwice = "";
+  let mtgcvaluesAlreadySeenThrice = "";
+  let mtgcvaluesAlreadySeenQuad = "";
+  let mtgcsecondresults = false;
+  let mtgcthirdresults = false;
+  let mtgcfourthresults = false;
+  var ygohfirstbox = "";
+  var ygohsecondbox = "";
+  var ygohthirdbox = "";
+  var ygohfourthbox = "";
+  let ygohvaluesAlreadySeenOnce = "";
+  let ygohvaluesAlreadySeenTwice = "";
+  let ygohvaluesAlreadySeenThrice = "";
+  let ygohvaluesAlreadySeenQuad = "";
+  let ygohsecondresults = false;
+  let ygohthirdresults = false;
+  let ygohfourthresults = false;
   const mtgcsystemfilter = data.filter((system) => {
     let i = 0;
     do {
       if (system.system === "mtgc" && system.Approved === "Y") {
         //if value has been seen once set results to true
-        firstresults = valuesAlreadySeenOnce.includes(
+        firstresults = mtgcvaluesAlreadySeenOnce.includes(
           JSON.stringify(system.name)
         );
-        valuesAlreadySeenOnce += JSON.stringify(system.name);
+        mtgcvaluesAlreadySeenOnce += JSON.stringify(system.name);
         if (
-          (firstresults = valuesAlreadySeenOnce.includes(
+          (firstresults = mtgcvaluesAlreadySeenOnce.includes(
             JSON.stringify(system.name)
           ))
         ) {
-          secondresults = valuesAlreadySeenTwice.includes(
+          mtgcsecondresults = mtgcvaluesAlreadySeenTwice.includes(
             JSON.stringify(system.name)
           );
-          valuesAlreadySeenTwice += JSON.stringify(system.name);
-          if (secondresults) {
-            thirdresults = valuesAlreadySeenThrice.includes(
+          mtgcvaluesAlreadySeenTwice += JSON.stringify(system.name);
+          if (mtgcsecondresults) {
+            mtgcthirdresults = mtgcvaluesAlreadySeenThrice.includes(
               JSON.stringify(system.name)
             );
-            valuesAlreadySeenThrice += JSON.stringify(system.name);
+            mtgcvaluesAlreadySeenThrice += JSON.stringify(system.name);
             mtgcsecondbox += system.name + "<br>";
             mtgcfirstbox = mtgcfirstbox.replace(system.name + "<br>", "");
 
-            if (thirdresults) {
-              fourthresults = valuesAlreadySeenQuad.includes(
+            if (mtgcthirdresults) {
+              mtgcfourthresults = mtgcvaluesAlreadySeenQuad.includes(
                 JSON.stringify(system.name)
               );
-              valuesAlreadySeenQuad += JSON.stringify(system.name);
+              mtgcvaluesAlreadySeenQuad += JSON.stringify(system.name);
               mtgcthirdbox += system.name + "<br>";
               mtgcsecondbox = mtgcsecondbox.replace(system.name + "<br>", "");
               mtgcsecondbox = mtgcsecondbox.replace(system.name + "<br>", "");
-              if (fourthresults) {
+              if (mtgcfourthresults) {
                 mtgcfourthbox += system.name + "<br>";
                 mtgcthirdbox = mtgcthirdbox.replace(system.name + "<br>", "");
                 mtgcthirdbox = mtgcthirdbox.replace(system.name + "<br>", "");
@@ -82,72 +85,41 @@ function showInfo(results) {
       }
     } while (i < system.length);
   });
-  if (mtgcfirstbox == 0) {
-    mtgcfirstbox += "Maybe You?";
-  }
-  if (mtgcsecondbox == 0) {
-    mtgcsecondbox += "Maybe You?";
-  }
-  if (mtgcthirdbox == 0) {
-    mtgcthirdbox += "Maybe You?";
-  }
-  if (mtgcfourthbox == 0) {
-    mtgcfourthbox += "Maybe You?";
-  }
-  document.getElementById("mtgdemobox1").innerHTML = mtgcfirstbox;
-  document.getElementById("mtgdemobox2").innerHTML = mtgcsecondbox;
-  document.getElementById("mtgdemobox3").innerHTML = mtgcthirdbox;
-  document.getElementById("mtgdemobox4").innerHTML = mtgcfourthbox;
-}
-
-function showInfo2(results) {
-  var data = results.data;
-  var ygohfirstbox = "";
-  var ygohsecondbox = "";
-  var ygohthirdbox = "";
-  var ygohfourthbox = "";
-  let valuesAlreadySeenOnce = "";
-  let valuesAlreadySeenTwice = "";
-  let valuesAlreadySeenThrice = "";
-  let valuesAlreadySeenQuad = "";
-  let secondresults = false;
-  let thirdresults = false;
-  let fourthresults = false;
   const ygohsystemfilter = data.filter((system) => {
     let i = 0;
     do {
       if (system.system === "ygoh" && system.Approved === "Y") {
         //if value has been seen once set results to true
-        firstresults = valuesAlreadySeenOnce.includes(
+        firstresults = ygohvaluesAlreadySeenOnce.includes(
           JSON.stringify(system.name)
         );
-        valuesAlreadySeenOnce += JSON.stringify(system.name);
+        ygohvaluesAlreadySeenOnce += JSON.stringify(system.name);
         if (
-          (firstresults = valuesAlreadySeenOnce.includes(
+          (firstresults = ygohvaluesAlreadySeenOnce.includes(
             JSON.stringify(system.name)
           ))
         ) {
-          secondresults = valuesAlreadySeenTwice.includes(
+          ygohsecondresults = ygohvaluesAlreadySeenTwice.includes(
             JSON.stringify(system.name)
           );
-          valuesAlreadySeenTwice += JSON.stringify(system.name);
-          if (secondresults) {
-            thirdresults = valuesAlreadySeenThrice.includes(
+          ygohvaluesAlreadySeenTwice += JSON.stringify(system.name);
+          if (ygohsecondresults) {
+            ygohthirdresults = ygohvaluesAlreadySeenThrice.includes(
               JSON.stringify(system.name)
             );
-            valuesAlreadySeenThrice += JSON.stringify(system.name);
+            ygohvaluesAlreadySeenThrice += JSON.stringify(system.name);
             ygohsecondbox += system.name + "<br>";
             ygohfirstbox = ygohfirstbox.replace(system.name + "<br>", "");
 
-            if (thirdresults) {
-              fourthresults = valuesAlreadySeenQuad.includes(
+            if (ygohthirdresults) {
+              ygohfourthresults = ygohvaluesAlreadySeenQuad.includes(
                 JSON.stringify(system.name)
               );
-              valuesAlreadySeenQuad += JSON.stringify(system.name);
+              ygohvaluesAlreadySeenQuad += JSON.stringify(system.name);
               ygohthirdbox += system.name + "<br>";
               ygohsecondbox = ygohsecondbox.replace(system.name + "<br>", "");
               ygohsecondbox = ygohsecondbox.replace(system.name + "<br>", "");
-              if (fourthresults) {
+              if (ygohfourthresults) {
                 ygohfourthbox += system.name + "<br>";
                 ygohthirdbox = ygohthirdbox.replace(system.name + "<br>", "");
                 ygohthirdbox = ygohthirdbox.replace(system.name + "<br>", "");
@@ -174,8 +146,24 @@ function showInfo2(results) {
   if (ygohfourthbox == 0) {
     ygohfourthbox += "Maybe You?";
   }
+  if (mtgcfirstbox == 0) {
+    mtgcfirstbox += "Maybe You?";
+  }
+  if (mtgcsecondbox == 0) {
+    mtgcsecondbox += "Maybe You?";
+  }
+  if (mtgcthirdbox == 0) {
+    mtgcthirdbox += "Maybe You?";
+  }
+  if (mtgcfourthbox == 0) {
+    mtgcfourthbox += "Maybe You?";
+  }
   document.getElementById("ygohdemobox1").innerHTML = ygohfirstbox;
   document.getElementById("ygohdemobox2").innerHTML = ygohsecondbox;
   document.getElementById("ygohdemobox3").innerHTML = ygohthirdbox;
   document.getElementById("ygohdemobox4").innerHTML = ygohfourthbox;
+  document.getElementById("mtgdemobox1").innerHTML = mtgcfirstbox;
+  document.getElementById("mtgdemobox2").innerHTML = mtgcsecondbox;
+  document.getElementById("mtgdemobox3").innerHTML = mtgcthirdbox;
+  document.getElementById("mtgdemobox4").innerHTML = mtgcfourthbox;
 }
