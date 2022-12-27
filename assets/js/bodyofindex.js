a09128dc85c2ff1af238cda0b728f9af56b037ea
merlinlcb <merlinlcb@merlinlcb.com>
2022-12-27 12:41:36 -0700

    upload refactor from outside elements

--- assets/js/bodyofindex.js
+++ assets/js/bodyofindex.js
@@ -1,172 +1,138 @@
-//Define URLs for Data Import
-var responses =
-  "https://docs.google.com/spreadsheets/d/1YC4vzjepcRs5wcna0ZZFEPCcWJHKru-iZzKY3uWwuE4/pub?output=csv";
-
-var psgsettings =
-  "https://docs.google.com/spreadsheets/d/1i2lAHd1gzd3G1-UGbj-MxwRmNSVcbSCF0DvjAD5Um50/pub?output=csv";
-
-//Import Settings from Details Sheet
-function init() {
-  Papa.parse(psgsettings, {
-    download: true,
-    header: true,
-    complete: getsettings,
-  });
-}
-//Begin Building settings as global variable
-currentsettings = [];
-function getsettings(results) {
-  var settings = results.data;
-  currentsettings = settings;
-  sortlogic();
-}
-//Wait for DOM load
-window.addEventListener("DOMContentLoaded", init);
-
-//Begin Sort Logic upon complete import of settings then parse End-User Input
-async function sortlogic(results) {
-  Papa.parse(responses, {
-    download: true,
-    header: true,
-    complete: newsort,
-  });
-
-  //Begin Sorting End-User Input
-  async function newsort(results) {
-    var settings = currentsettings;
-    var data = results.data;
-    var firstbox = "";
-    var secondbox = "";
-    var thirdbox = "";
-    var fourthbox = "";
-    let valuesAlreadySeenOnce = "";
-    let valuesAlreadySeenTwice = "";
-    let valuesAlreadySeenThrice = "";
-    let valuesAlreadySeenQuad = "";
-    let secondresults = false;
-    let thirdresults = false;
-    let fourthresults = false;
-    var activeraces = [];
-    var divids = [];
-    //Check information from Global Settings variable to filter by active racecodes
-    const settingsbuilder = settings.filter((newsettings) => {
-      let i = 0;
-      do {
-        if (newsettings.active === "Y") {
-          activeraces.push(newsettings.race_code);
-        }
-      } while (i < newsettings.length);
-    });
-    //Logic for Filtering Position in race
-    const systemfilter = data.filter((system) => {
-      let z = 0;
-      do {
-        numbertoloop = 0;
-        ApprovedRacers = [];
-        CurrentRace = [];
-        let target = system.name;
-
-        data.forEach((system) => {
-          for (let key in system) {
-            // system.keys(name);
-            // console.log(`${key}: ${system[key]}`);
-            cities
-              .filter((city) => city.population < 3000000)
-              .sort((c1, c2) => c1.population - c2.population)
-              .map((city) => console.log(city.name + ":" + city.population));
-            console.log(Object.values(system[3]));
-            // console.log(system[key.name]);
-          }
-        });
-
-        // let i = 0;
-        // do {
-        //   {
-        //     divids = divids += system.racecode;
-        //     if (system.system === "mtg") {
-        //       headerimage = "assets/images/MTGWhite.webp";
-        //     }
-        //     if (system.system === "ygoh") {
-        //       headerimage = "assets/images/Yugioh_Logo.webp";
-        //     }
-        //     if (system.system === "boardgames") {
-        //       headerimage = "assets/images/Game_Night_Logo_Color.webp";
-        //     }
-        //     if (system.system === "warhammer") {
-        //       headerimage = "assets/images/Warhammer-40K-Logo.webp";
-        //     }
-        //     if (system.system === "dungeonsanddragons") {
-        //       headerimage = "assets/images/dd-logo.webp";
-        //     }
-        //     if (system.system === "vanguard") {
-        //       headerimage = "assets/images/CFVG.webp";
-        //     }
-        //     if (system.system === "pathfinder") {
-        //       headerimage = "assets/images/pathfinder-rpg-logo.webp";
-        //     }
-        //     if (system.Approved === "Y") {
-        //       //if value has been seen once set results to true
-        //       firstresults = valuesAlreadySeenOnce.includes(
-        //         JSON.stringify(system.name)
-        //       );
-        //       valuesAlreadySeenOnce += JSON.stringify(system.name);
-        //       if (
-        //         (firstresults = valuesAlreadySeenOnce.includes(
-        //           JSON.stringify(system.name)
-        //         ))
-        //       ) {
-        //         secondresults = valuesAlreadySeenTwice.includes(
-        //           JSON.stringify(system.name)
-        //         );
-        //         valuesAlreadySeenTwice += JSON.stringify(system.name);
-        //         if (secondresults) {
-        //           thirdresults = valuesAlreadySeenThrice.includes(
-        //             JSON.stringify(system.name)
-        //           );
-        //           valuesAlreadySeenThrice += JSON.stringify(system.name);
-        //           secondbox += system.name + "<br>";
-        //           firstbox = firstbox.replace(system.name + "<br>", "");
-
-        //           if (thirdresults) {
-        //             fourthresults = valuesAlreadySeenQuad.includes(
-        //               JSON.stringify(system.name)
-        //             );
-        //             valuesAlreadySeenQuad += JSON.stringify(system.name);
-        //             thirdbox += system.name + "<br>";
-        //             secondbox = secondbox.replace(system.name + "<br>", "");
-        //             secondbox = secondbox.replace(system.name + "<br>", "");
-        //             if (fourthresults) {
-        //               fourthbox += system.name + "<br>";
-        //               thirdbox = thirdbox.replace(system.name + "<br>", "");
-        //               thirdbox = thirdbox.replace(system.name + "<br>", "");
-        //               thirdbox = thirdbox.replace(system.name + "<br>", "");
-        //             }
-        //           }
-        //         }
-        //       }
-        //     }
-        //   } else {
-        //     firstbox += system.name + "<br>";
-        //   }
-        //   if (firstbox == 0) {
-        //     firstbox += "Maybe You?";
-        //   }
-        //   if (secondbox == 0) {
-        //     secondbox += "Maybe You?";
-        //   }
-        //   if (thirdbox == 0) {
-        //     thirdbox += "Maybe You?";
-        //   }
-        //   if (fourthbox == 0) {
-        //     fourthbox += "Maybe You?";
-        //   }
-        //   console.log("system = " + system.system + " firstbox = ");
-        //   i++;
-        // } while (i < system.length);
-
-        z++;
-      } while (z < activeraces.length);
-    });
-    document.getElementById("innercontent").innerHTML = fourthbox;
+const getObjData = async () => {
+  const response = await fetch(
+    "https://docs.google.com/spreadsheets/d/1YC4vzjepcRs5wcna0ZZFEPCcWJHKru-iZzKY3uWwuE4/pub?output=csv"
+  );
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
   }
-}
+  return response.blob();
+};
+
+const getSettingData = async () => {
+  const response = await fetch(
+    "https://docs.google.com/spreadsheets/d/1i2lAHd1gzd3G1-UGbj-MxwRmNSVcbSCF0DvjAD5Um50/pub?output=csv"
+  );
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return response.blob();
+};
+
+const getObjJSON = async () => {
+  let returnableResults;
+  await getObjData()
+    .then((blob) => {
+      return blob.text();
+    })
+    .then((csv) => {
+      Papa.parse(csv, {
+        header: true,
+        complete: (results) => {
+          returnableResults = results;
+        },
+      });
+    });
+  return returnableResults;
+};
+
+const getSettingJSON = async () => {
+  let returnableResults;
+  await getSettingData()
+    .then((blob) => {
+      return blob.text();
+    })
+    .then((csv) => {
+      Papa.parse(csv, {
+        header: true,
+        complete: (results) => {
+          returnableResults = results;
+        },
+      });
+    });
+  return returnableResults;
+};
+
+Promise.all([getSettingJSON(), getObjJSON()]).then(
+  ([raceResults, memberResults]) => {
+    /*console.log("Race Results", raceResults.data);*/ // debug point
+    console.log("MemberResults", memberResults.data); // debug point
+    // add a "total" property to each race in the activeRaces array
+    raceResults = raceResults.data.map((race) => ({ ...race, total: 0 }));
+    memberResults.data.forEach((member) => {
+      const i = raceResults.findIndex(
+        (race) => race["race_code"] === member.racecode
+      );
+      console.log(i);
+      raceResults[i].total++;
+    });
+    raceResults = raceResults.sort((a, b) => b.total - a.total);
+    console.log("active races", raceResults); // debug point
+
+    raceResults.forEach((race) => {
+      if (
+        race.total > 0 ||
+        race.active === "Y" ||
+        race["name_of_race"] !== ""
+      ) {
+        const div = document.createElement("div");
+        const title = document.createElement("h2");
+        const memberContainer = document.createElement("div");
+        title.textContent = race["name_of_race"];
+        div.id = race["race_code"];
+        div.appendChild(title);
+        div.appendChild(memberContainer);
+        div.dataset.count = "0"; // this could be useful in the future
+        //add a solid thin black border to the div
+        div.style.border = "1px solid black";
+        div.style.margin = "10px 0";
+        div.style.padding = "10px";
+        document.getElementById("raceContainer").appendChild(div);
+      }
+    });
+
+    memberResults.data.forEach((member) => {
+      const div = document.createElement("div");
+      const name = document.createElement("p");
+      name.textContent = member["name"];
+      div.appendChild(name);
+      document.getElementById(member.racecode).appendChild(div);
+    });
+  }
+);
+
+/**
+ * Array Key value compare and count.
+ *
+ * Count the number of times each key appears in the array based on the keyArray.
+ *
+ * @param {Object[]} objArray The array of object from which you need to compare.
+ * @param {String[]} keyArray Keys to count against.
+ * @param {string} propName The property name to compare against.
+ * @returns {Object[]} The array of object properties with their respective totals.
+ */
+const countOccurrencesInArray = (objArray, keyArray, propName) => {
+  // Get totals in put them into an array
+  const totals = keyArray.map((key) => {
+    return objArray.reduce((acc, obj) => {
+      return obj[propName] === key ? acc + 1 : acc;
+    }, 0);
+  });
+
+  // match the original property values with the totals and put that into array of objects
+  return keyArray.map((key, index) => {
+    return { [key]: totals[index] };
+  });
+};
+
+/**
+ * Sort an array based on the value of a single object of a property value.
+ * Ensure that higher values are first.
+ *
+ * @param {Object[]} arr The array of object to sort.
+ * @returns {Object[]} The sorted array of object.
+ */
+const sortArray = (arr) => {
+  return arr.sort((a, b) => {
+    return Object.values(b)[0] - Object.values(a)[0];
+  });
+};
